2b7cfc35a138681a36a3ad698e8576928643128b
Sourabh Gaikwad <sourabhgaikwad2399@gmail.com>
2021-11-01 14:49:32 +0530

    First Version

--- testing1.java
+++ testing1.java
@@ -1,7 +1,10 @@
 class testing1
 {
-  public static void main(string args[])
+  public static void main(String args[])
   {
     System.out.println("Hello This is Git Here");
+   //System.out.println("Practicing Git Commit after changes");
+    testing2 t = new testing2();
+    t.Display();
   }
 }
---
+++ testing2.java
@@ -0,0 +1,9 @@
+
+public class testing2 {
+
+	public void Display()
+	{
+		System.out.println("testing2 class : Called from main");
+	}
+
+}
